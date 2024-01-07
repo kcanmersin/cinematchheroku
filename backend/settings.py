a44719dd518cef820768f7df300e56d8c374f8f0
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2024-01-07 22:25:04 +0300

    .

--- backend/settings.py
+++ backend/settings.py
@@ -125,11 +125,19 @@ DATABASES = {
 }
 
 import dj_database_url
+DATABASES['default'] = dj_database_url.parse('postgres://cinematchfr_user:KVKMAal90pm4OIde4IcqbUsIBYfvrAoP@dpg-cmdfeuo21fec73d33khg-a.frankfurt-postgres.render.com/cinematchfr')
 
 
-DATABASES['default'] = dj_database_url.parse('postgres://corleone_user:dQ3loGRYkKNvFXupiXcpRUjmZBw4JXbC@dpg-cmbgi20cmk4c73dgp4pg-a.oregon-postgres.render.com/corleone')
-
-# postgres://corleone_user:dQ3loGRYkKNvFXupiXcpRUjmZBw4JXbC@dpg-cmbgi20cmk4c73dgp4pg-a.oregon-postgres.render.com/corleone
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+#         "USER": os.environ.get("SQL_USER", "user"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+#         "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -140,17 +148,6 @@ DATABASES['default'] = dj_database_url.parse('postgres://corleone_user:dQ3loGRYk
 # #        'HOST': 'w3-django-project.cdxmgq9zqqlr.us-east-1.rds.amazonaws.com',
 #         'HOST': 'localhost',
 #         'PORT': '5432'
-#     }
-# }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-#         "USER": os.environ.get("SQL_USER", "user"),
-#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#         "HOST": os.environ.get("SQL_HOST", "localhost"),
-#         "PORT": os.environ.get("SQL_PORT", "5432"),
 #     }
 # }
 
