702603f23a3999d86be7af237f9448c83b78a241
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2024-01-10 23:59:51 +0300

    fix serilizer

--- APImovie/serializers.py
+++ APImovie/serializers.py
@@ -16,7 +16,7 @@ class MovieSForYouSerializer(serializers.ModelSerializer):
         
     class Meta:
             model = Movie
-            fields = ('id', 'title', 'poster_path','release_date',"background_path")
+            fields = ('id', 'title', 'poster_path','release_date',"background_path","vote_average")
 
   
 class UserProfileSerializer(serializers.ModelSerializer):
