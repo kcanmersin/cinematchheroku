ef0bf00fa7e026755915abbb316b4b8a22f99978
Erenayyilmaz <74668808+Erenayyilmaz@users.noreply.github.com>
2024-01-11 15:14:18 +0300

    follower serializer profile photo added

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,20 +2,58 @@ from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 from .models import Follower, UserProfile
 
+# class FollowerSerializer(serializers.ModelSerializer):
+#     user = serializers.DictField(child=serializers.CharField(), source='get_user_info', read_only=True)
+#     is_followed_by = serializers.DictField(child=serializers.CharField(), source='get_is_followed_by_info', read_only=True)
+
+#     class Meta:
+#         model = Follower
+#         fields = ('user', 'is_followed_by')
+#         read_only_fields = ('user', 'is_followed_by')
+
+#     def to_representation(self, instance):
+#         data = super().to_representation(instance)
+#         if data.get('user') and data.get('is_followed_by'):
+# #            print(data)
+#             #data['user']['profile_photo'] = instance.user.profile_photo.url if instance.user.profile_photo else None
+#             return data
+#         return None
+from rest_framework import serializers
+from .models import Follower
+
 class FollowerSerializer(serializers.ModelSerializer):
-    user = serializers.DictField(child=serializers.CharField(), source='get_user_info', read_only=True)
-    is_followed_by = serializers.DictField(child=serializers.CharField(), source='get_is_followed_by_info', read_only=True)
+    user = serializers.SerializerMethodField()
+    is_followed_by = serializers.SerializerMethodField()
 
     class Meta:
         model = Follower
         fields = ('user', 'is_followed_by')
-        read_only_fields = ('user', 'is_followed_by')
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if data.get('user') and data.get('is_followed_by'):
             return data
         return None
+
+    def get_user(self, instance):
+        user = instance.user
+        user_profile = user.profile.first()  
+
+        return {
+            'id': user.id,
+            'username': user.username,
+            'profile_picture': self.context['request'].build_absolute_uri(user_profile.profile_picture.url) if user_profile and user_profile.profile_picture else None,
+        }
+
+    def get_is_followed_by(self, instance):
+        is_followed_by = instance.is_followed_by
+        is_followed_by_profile = is_followed_by.profile.first()  # Access the UserProfile directly
+
+        return {
+            'id': is_followed_by.id,
+            'username': is_followed_by.username,
+            'profile_picture': self.context['request'].build_absolute_uri(is_followed_by_profile.profile_picture.url) if is_followed_by_profile and is_followed_by_profile.profile_picture else None,
+        }
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -43,7 +81,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 #        current_user_profile = self.context['request'].user.profile.first()
         current_user_profile = obj.user.profile.first()
         if current_user_profile:
-            print(current_user_profile)
+            # print(current_user_profile)
             return current_user_profile.get_watched_movie_count()
         return 0
 
